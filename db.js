c022ee7b0447c6b2c31daaa1b97f15985436acb5
basON <wch.basinski@gmail.com>
2022-12-19 23:03:23 +0100

    Add post delete and put order

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const db = [
   { id: 1, name: "Soup", price: 11 },
-  { id: 1, name: "Pizza", price: 27 },
-  { id: 1, name: "Pasta", price: 15 },
+  { id: 2, name: "Pizza", price: 27 },
+  { id: 3, name: "Pasta", price: 15 },
 ];
 module.exports = db;
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -17,5 +17,63 @@ router.get("/:id", async function (req, res, next) {
     res.status(400).json({ message: "Order has not been found." });
   }
 });
+/*POST Add order */
+router.post("/", async function (req, res, next) {
+  console.log(req.body);
+
+  if (!req.body.name || !req.body.price) {
+    req.status(400).json({ message: "Name or price is missing." });
+    return;
+  }
+  const order = {
+    id: db.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+  };
+  db.push(order);
+
+  res.json(order);
+  return;
+});
+
+/*DELETE Delete order */
+router.delete("/:id", async function (req, res, next) {
+  const id = req.params.id;
+  console.log(req.params.id);
+  //find id of the element in the array
+  const indexOfObject = db.findIndex((object) => {
+    return object.id == req.params.id;
+  });
+  if (indexOfObject == -1) {
+    res
+      .status(400)
+      .json({ message: "Order with the given id does not exist." });
+    return;
+  }
+  //delete element from the "db"
+  db.splice(indexOfObject, 1);
+  res.json(db[indexOfObject]);
+});
+/*PUT Update order */
+router.put("/", async function (req, res, next) {
+  if (!req.body.name || !req.body.price || !req.body.id) {
+    res.status(400).json({ message: "Name or price or id is missing." });
+    return;
+  }
+  //find id of the element in the array
+  const indexOfObject = db.findIndex((object) => {
+    return object.id == req.body.id;
+  });
+  if (indexOfObject == -1) {
+    res
+      .status(400)
+      .json({ message: "Order with the given id does not exist." });
+    return;
+  }
+  db[indexOfObject].name = req.body.name;
+  db[indexOfObject].price = req.body.price;
+
+  res.json(db[indexOfObject]);
+});
 
 module.exports = router;
